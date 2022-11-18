6e3aa919510ba91f921de509ecb6e57aa531a828
Bain <Bain@qq.com>
2022-11-18 19:42:18 +0800

    hot-fix 4 commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git第一次");
         System.out.println("git第二次");
         System.out.println("git第三次");
+        System.out.println("hot-fix git第四次");
     }
 }
